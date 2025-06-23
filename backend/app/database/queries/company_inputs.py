c9329c3ed88c8222af8d542e0b2c7214e2cf61e2
khalid <ga.lid2002.km@gmail.com>
2025-06-23 09:28:48 +0100

    feat: add additional database queries with relevant tests

--- backend/app/database/queries/company_inputs.py
+++ backend/app/database/queries/company_inputs.py
@@ -9,4 +9,39 @@ async def get_cooling_maintenance_rate(country: str):
     country_overrides = document.get("country_overrides", {})
     if country in country_overrides:
         return country_overrides[country]
-    return document.get("value", 0.08)
+    return document.get("value")
+
+async def get_budget_IT_energy():
+    db = get_db()
+    collection = db["company_inputs"]
+    
+    document = await collection.find_one({"name": "budgeted_it_energy_consumption"})
+    return document.get("value")
+    
+async def get_budget_fan_energy():
+    db = get_db()
+    collection = db["company_inputs"]
+    
+    document = await collection.find_one({"name": "budgeted_fan_energy_consumption"})
+    return document.get("value")
+
+async def get_actual_fan_power():
+    db = get_db()
+    collection = db["company_inputs"]
+
+    document = await collection.find_one({"name": "actual_fan_power_consumption"})
+    return document.get("value")
+
+async def get_water_price_per_litre():
+    db = get_db()
+    collection = db["company_inputs"]
+
+    document = await collection.find_one({"name": "water_price_per_litre"})
+    return document.get("value")
+
+async def get_water_use_per_kwh():
+    db = get_db()
+    collection = db["company_inputs"]
+
+    document = await collection.find_one({"name": "water_use_per_kwh"})
+    return document.get("value")
