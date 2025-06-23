a4d6c7bfd28880b0543fea8603aba0d7ab6908fc
khalid <ga.lid2002.km@gmail.com>
2025-06-23 10:35:28 +0100

    feat: added extra queries regarding country rates

---
+++ backend/app/database/queries/country_rates.py
@@ -0,0 +1,22 @@
+from app.database.connection import get_db
+
+async def get_co2e_intensity_per_kwh(country: str):
+    db = get_db()
+    collection = db["countries"]
+
+    document = await collection.find_one({"country": country})
+    if document is None:
+        return None
+    return document.get("co2e_intensity_g_per_kwh")
+
+async def get_cooling_capex_rate(country: str):
+    db = get_db()
+    collection = db["countries"]
+
+    document = await collection.find_one({"country": country})
+    if document is None:
+        return None
+    return document.get("cooling_capex_rate_per_kw")
+
+    
+    
