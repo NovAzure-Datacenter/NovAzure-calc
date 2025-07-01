8d8b9dc79e71b6e618731f45c5e124571885eefe
Leonardo Revrenna <revrennaleo@gmail.com>
2025-07-01 19:21:55 +0100

    Moves it-cong-adv functions to it-config in solutions/calculations

--- backend/app/services/calculations/it_config.py
+++ backend/app/services/calculations/it_config.py
@@ -12,3 +12,15 @@ def calculate_server_rated_max_power(data_center_type: str){
     else:
         return None 
 }
+"""
+Number of Server Refreshes Calculation Service
+"""
+
+def calculate_number_of_server_refreshes(planned_years: int){
+    if planned_years >= 5:
+        return math.ceil((planned_years / 5) - 1)
+    elif planned_years == "HPC/AI":
+        return 2
+    elif planned_years < 5:
+        return 0
+}
