b858dc128472636428538882e84d3fcd48c15125
khalid <ga.lid2002.km@gmail.com>
2025-07-21 13:05:47 +0100

    feat: Implement Parameter class for calculation model

--- backend/app/core/calculation/parameter.py
+++ backend/app/core/calculation/parameter.py
@@ -1,1 +1,16 @@
 # Parameter model
+
+
+class Parameter:
+    def __init__(self, data: dict):
+        self.name = data["name"]
+        self.type = data["type"] # COMPANY/USER/CALCULATION/GLOBAL
+        self.unit = data.get('unit', '')
+        self.value = data.get('value') # For static parameters only (GLOBAL/COMPANY)
+        self.formula = data.get('formula') # For CALCULATION parameters only
+        self.dependencies = data.get('dependencies', [])
+        
+        # Runtime states
+        self.ast = None
+        self.evaluated = False
+        self.result = None
