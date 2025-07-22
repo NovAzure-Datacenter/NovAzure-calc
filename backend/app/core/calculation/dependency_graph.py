26052bb2d003127835d3085b9d7b21600cc01f59
khalid <ga.lid2002.km@gmail.com>
2025-07-22 13:01:36 +0100

    feat: Add topological sort to DependencyGraph class

--- backend/app/core/calculation/dependency_graph.py
+++ backend/app/core/calculation/dependency_graph.py
@@ -1,11 +1,12 @@
 # DAG construction and topological sort
 from typing import Dict, List
+from collections import deque
 
 from app.core.calculation.parameter import Parameter
 
 
 class DependencyGraph:
-    def __init__(self, parameters: list["Parameter"]):
+    def __init__(self, parameters: list[Parameter]):
         self.parameters = parameters
         self.graph = self.build_graph()
 
@@ -17,3 +18,27 @@ class DependencyGraph:
             else:
                 graph[param.name] = []
         return graph
+        
+    def topological_sort(self) -> List[str]:
+        in_degree = {node: 0 for node in self.graph}
+        for node, deps in self.graph.items():
+            for dep in deps:
+                in_degree[node] += 1
+        
+        queue = deque([node for node, degree in in_degree.items() if degree == 0])
+        sorted_result = []
+        
+        while queue:
+            node = queue.popleft()
+            sorted_result.append(node)
+            
+            for dependant in self.graph:
+                if node in self.graph[dependant]:
+                    in_degree[dependant] -= 1
+                    if in_degree[dependant] == 0:
+                        queue.append(dependant)
+        
+        if len(sorted_result) != len(self.graph):
+            raise ValueError("Cycle detected in parameter dependencies")
+        
+        return sorted_result 
