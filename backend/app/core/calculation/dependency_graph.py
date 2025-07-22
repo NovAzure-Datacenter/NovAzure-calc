0bea45dabbd8acebc2bae3b8d5518f2f5d74d00c
khalid <ga.lid2002.km@gmail.com>
2025-07-22 12:35:44 +0100

    feat: DAG implementation started with class definition

--- backend/app/core/calculation/dependency_graph.py
+++ backend/app/core/calculation/dependency_graph.py
@@ -1,1 +1,19 @@
 # DAG construction and topological sort
+from typing import Dict, List
+
+from app.core.calculation.parameter import Parameter
+
+
+class DependencyGraph:
+    def __init__(self, parameters: list["Parameter"]):
+        self.parameters = parameters
+        self.graph = self.build_graph()
+
+    def build_graph(self) -> Dict[str, List[str]]:
+        graph = {}
+        for param in self.parameters:
+            if param.type == "CALCULATION":
+                graph[param.name] = param.dependencies
+            else:
+                graph[param.name] = []
+        return graph
