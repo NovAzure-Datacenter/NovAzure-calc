0f4b28becfb04dfdb6af2cacfd411175a20bce3c
Leonardo Revrenna <revrennaleo@gmail.com>
2025-08-23 17:34:10 +0100

    Removes sidebar from auth pages

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -19,6 +19,7 @@ export default async function DashboardLayout({
 
 	return (
 		<DashboardClientWrapper>
+			<CustomSidebar />
 			<SidebarInset className="flex h-screen overflow-x-hidden   bg-gradient-to-br from-blue-50 to-sky-50 relative">
 				{children}
 			</SidebarInset>
