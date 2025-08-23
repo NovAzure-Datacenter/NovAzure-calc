6f5e3d72dd20ed46720815e4b7a9b907521ee53f
Leonardo Revrenna <revrennaleo@gmail.com>
2025-08-23 17:03:46 +0100

    Adds redirect to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,14 @@
-import _CTA from "./_landing/components/cta/cta";
-import _DataSecurity from "./_landing/components/data-security/data-security";
-import _Features from "./_landing/components/features/features";
-import Footer from "./_landing/components/footer/footer";
-import Hero from "./_landing/components/hero/hero";
-import _CaseStudies from "./_landing/components/case-studies/case-studies";
-import _Stats from "./_landing/components/stats/stats";
-import _Testimonials from "./_landing/components/testimonials/testimonials";
-import Navbar from "@/components/navbar/navbar";
-import UnderConstruction from "@/components/under-construction";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function Home() {
-	return (
-		<div className="flex flex-col items-center w-full">
-			{/* <Navbar />
-			<Hero /> */}
-			{/* <Features />
-			<DataSecurity />
-			<Stats />
-			<CaseStudies />
-			<Testimonials />
-			<CTA /> */}
-			{/* <Footer /> */}
-			<UnderConstruction/>
-		</div>
-	);
+export default async function Home() {
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		redirect("/login");
+	}else{
+		redirect("/home/dashboard");
+	}
+	
 }
